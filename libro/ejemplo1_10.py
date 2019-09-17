89fc75b0bbf608c59ca7f7cb1caf1d467fc88a3b
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-09-16 23:12:45 -0500

    problema 1.10 del libro

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,7 @@
+print("programa que calcula el area y perimetro de un cilindro")
+rad = int(input("cual es el radio: "))
+alt = int(input("cual es la altura: "))
+vol = (3.141592)*(rad**2)*(alt)
+are = 2*3.141592*rad*alt
+print("el area del cilindro es {} y su volumen es {}".format (are,vol))
+
