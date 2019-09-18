15a49797c81a2267473a52b53066c81ffa3ef226
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-09-18 17:49:03 -0500

    programa 1.11 del libro

---
+++ libro/programa1_11.py
@@ -0,0 +1,6 @@
+# programa que invierte los datos dados por el usuario
+a= input("dame el primer dato: ")
+b= ("dame el segundo dato: ")
+c= ("dame el tercer dado: ")
+d= ("dame el cuarto dato: ")
+print("{}{}{}{}".format(d,c,b,a))
