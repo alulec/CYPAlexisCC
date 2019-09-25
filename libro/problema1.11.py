d16b286dd518b478e92dd48039056803791b6be9
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-09-24 21:59:26 -0500

    ...

--- libro/problema1.11.py
+++
@@ -1,4 +0,0 @@
-print("Programa que a partir de una cantidad de dias dados, calcula cuantos segundos hay en dicha cantidad de dias.")
-dias= int(input("¿cuantos dias?: "))
-seg= dias*24*60*60
-print("En ",dias," hay ",seg," segundos.")
