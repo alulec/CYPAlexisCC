8dcae5ff712802c822c7758b9c6da853790f7cb4
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-10-10 17:56:22 -0500

    funcion for

---
+++ for.py
@@ -0,0 +1,4 @@
+v = 0
+for v in range(1 , 11 , 1):
+    t = v * 5
+    print("5 * ", v , "= ", t)
---
+++ for_2.py
@@ -0,0 +1,5 @@
+v = 0
+tabla = float(input("Que tabla?: "))
+for v in range( 1 , 11 , 1 ):
+    print(f"{tabla} x {v} = {tabla * v}")
+              
