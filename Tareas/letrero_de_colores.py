692952af5d14d52c69b9ab101b4153113306006a
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-11-14 11:43:26 -0600

    tarea 1

---
+++ Tareas/letrero_de_colores.py
@@ -0,0 +1,6 @@
+# banner de colores
+from asciistuff import Banner
+from stringcolor import *
+print(cs("Universidad Autónoma de Mexico","gold"))
+print(cs("FES Aragon","blue4"))
+print(cs("Cordova de la Cruz Alexis Jesus","lightgreen"))
