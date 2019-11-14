897c646a29700c36162b8a2c159894d72a77b50c
alulec <alexisjesuscordovadelacruz@gmail.com>
2019-11-14 12:10:39 -0600

    1

--- leer_numeros.py
+++ leer_numeros.py
@@ -3,19 +3,25 @@ archivo = open("numeros.txt","rt")
 
 numeros1 = archivo.read()
 print(numeros1)
-print(numeros1.split("\n"))
+print(numeros1.split('\n'))
 lista_num=[]
-for linea in numeros1.split("\n"):
+for linea in numeros1.split('\n'):
     for numero in linea.split(','):
         lista_num.append( int(numero))
 print(lista_num)
-lista_sum.sort()
-print(f"\n Lista ordenada: {lista_num}")
+lista_num.sort()
+print(f"\n Lista ordenada: {lista_num} \n")
 
 print(f"El mayor es : {lista_num[-1]} y el menor es: {lista_num[0]}")
 archivo.close()
 
-archivo= open("numeros.txt","rt")
-numeros2 = archivo.readline()
+archivo = open("numeros.txt","rt")
+numeros2 = archivo.readlines()
 print(numeros2)
 archivo.close()
+
+archivo = open("numeros.txt","rt")
+numeros2 = archivo.readlines()
+print(numeros2)
+archivo.close()
+
